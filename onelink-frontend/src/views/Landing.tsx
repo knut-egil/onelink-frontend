a35542ab961e547b271816c5270c77d98303b4a5
ninjasploit <ninjasploit@users.noreply.github.com>
2024-02-01 13:01:57 +0100

    Basic landing-page w register button

---
+++ onelink-frontend/src/views/Landing.tsx
@@ -0,0 +1,31 @@
+import "./Landing.css";
+function Landing() {
+  return (
+    <>
+      <div className="page">
+        <div className="container">
+          <h1>OneLink</h1>
+          <p>
+            Easily share all your social links, with one link!
+            <br />
+            ~~~
+            <br />
+            Get insights into how many visitors you get and who your visitors
+            are.
+            <br />
+            ~~~
+            <br />
+            Figure out what generates the most traffic to your socials by
+            quickly looking at your visitors graphs.
+          </p>
+          <br />
+          <a href="/register" className="link">
+            Create an account
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Landing;
