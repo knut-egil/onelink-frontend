6187e2c1626f8d4ce81b250135c4c2d67493a128
ninjasploit <ninjasploit@users.noreply.github.com>
2024-02-01 13:53:45 +0100

    imrpove form style and add onSubmit handle

--- onelink-frontend/src/views/Register.tsx
+++ onelink-frontend/src/views/Register.tsx
@@ -1,17 +1,75 @@
+import { useState } from "react";
 import "./Register.css";
 function Register() {
+  const [username, setUsername] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [confirmPassword, setConfirmPasword] = useState<string>();
+
+  async function onSubmit(event: React.FormEvent) {
+    // Prevent any accidental auto-form submissions!
+    event.preventDefault();
+
+    // Custom submission logic
+    const payload = {
+      username: username,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+
+    // Log
+    console.log(
+      `Created account! Payload: ${JSON.stringify(payload, null, 2)}`
+    );
+
+    try {
+      // Make register requests
+      const res = await fetch(
+        "https://webhook.site/e610e775-96bc-48b7-9802-c49a627ce27b/api/register",
+        {
+          method: "post",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+
+      // Check ok status
+      if (!res.ok) throw new Error("Non-ok status!");
+
+      // Get response data
+      const data = await res.json();
+
+      // Handle success :)
+      console.log(
+        `Account creation successful, data: ${JSON.stringify(data, null, 2)}`
+      );
+    } catch (err) {
+      // Failed register
+      const { stack, message } = err as Error;
+      console.error(`Account creation failed, error: ${stack ?? message}`);
+
+      // Tell the user it failed!
+    }
+  }
+
   return (
     <>
       <div className="page">
         <div className="container">
           <div className="title">Create Account</div>
-          <form action="">
+          <form onSubmit={onSubmit}>
             <label htmlFor="username">Username</label>
             <input
               type="text"
               name="username"
               id="username"
               placeholder="john.doe"
+              onChange={(e) => {
+                setUsername(e.currentTarget.value);
+              }}
               required
             />
 
@@ -21,6 +79,9 @@ function Register() {
               name="email"
               id="email"
               placeholder="john@doe.com"
+              onChange={(e) => {
+                setEmail(e.currentTarget.value);
+              }}
               required
             />
 
@@ -30,22 +91,29 @@ function Register() {
               name="password"
               autoComplete="new-password"
               placeholder="password"
+              onChange={(e) => {
+                setPassword(e.currentTarget.value);
+              }}
               required
             />
 
-            <label htmlFor="password">Confirm password</label>
+            <label htmlFor="confirmPassword">Confirm password</label>
             <input
+              id="confirmPassword"
               type="password"
-              name="password"
+              name="confirmPassword"
               autoComplete="new-password"
               placeholder="password"
+              onChange={(e) => {
+                setConfirmPasword(e.currentTarget.value);
+              }}
               required
             />
+            <br />
+            <button type="submit" className="link">
+              Register
+            </button>
           </form>
-          <br />
-          <a href="/register" className="link">
-            Register
-          </a>
         </div>
       </div>
     </>
