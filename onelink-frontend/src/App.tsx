64efbef017310ba3534876a6ee52c3eff9e49673
ninjasploit <ninjasploit@users.noreply.github.com>
2024-02-01 12:35:49 +0100

    Add landing

--- onelink-frontend/src/App.tsx
+++ onelink-frontend/src/App.tsx
@@ -1,37 +1,47 @@
 import "./App.css";
+import Landing from "./views/Landing";
 import Profile from "./views/Profile";
 
 function App() {
+  /**
+   * Show test profile bool
+   */
+  const ShowProfile: boolean = false;
+  const TestProfile = (
+    <Profile
+      links={[
+        {
+          id: "0",
+          url: "https://instagram.com/knutegilvn",
+          title: "// Instagram",
+          icon: "instagram",
+        },
+        {
+          id: "1",
+          url: "https://github.com/knut-egil",
+          title: "// GitHub",
+          icon: "github",
+        },
+        {
+          id: "2",
+          url: "https://discordapp.com/users/203526947601383424",
+          title: "// Discord",
+          icon: "discord",
+        },
+        {
+          id: "3",
+          url: "javascript:alert(1)",
+          title: "// Implement",
+          icon: "#",
+        },
+      ]}
+    />
+  );
+
   return (
     <>
-      <Profile
-        links={[
-          {
-            id: "0",
-            url: "https://instagram.com/knutegilvn",
-            title: "// Instagram",
-            icon: "instagram",
-          },
-          {
-            id: "1",
-            url: "https://github.com/knut-egil",
-            title: "// GitHub",
-            icon: "github",
-          },
-          {
-            id: "2",
-            url: "https://discordapp.com/users/203526947601383424",
-            title: "// Discord",
-            icon: "discord",
-          },
-          {
-            id: "3",
-            url: "javascript:alert(1)",
-            title: "// Implement",
-            icon: "#",
-          },
-        ]}
-      />
+      <Landing></Landing>
+      {ShowProfile && TestProfile}
     </>
   );
 }
