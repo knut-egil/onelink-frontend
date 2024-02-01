a3dd3c55f234ffd4996c1c8dc9a61c3404cd83b7
ninjasploit <ninjasploit@users.noreply.github.com>
2024-02-01 10:26:16 +0100

    Create profile view w finished header

--- onelink-frontend/src/views/Profile.tsx
+++ onelink-frontend/src/views/Profile.tsx
@@ -1,10 +1,44 @@
 import "./Profile.css";
 
+function Header(props: {
+  profileImage: string;
+  title: string;
+  bio: string;
+  extras: string[];
+}) {
+  return (
+    <>
+      <div className="header">
+        <div className="picture">
+          <img src={props.profileImage}></img>
+        </div>
+        <div className="about">
+          <span className="title">{props.title}</span>
+          <span className="bio">{props.bio}</span>
+          <div className="extra">
+            <span>{props.extras.join(" - ")}</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
 function Profile() {
   return (
     <>
-      <h1>Profile</h1>
-      // Implement
+      <div className="container">
+        <Header
+          profileImage={"https://picsum.photos/200"}
+          title={"Knut Egil, 22yo"}
+          bio={
+            "Check out my other social channels by clicking the buttons below!"
+          }
+          extras={["Software Developer", "Norway"]}
+        />
+        // Implement
+        <div className="link-container"></div>
+      </div>
     </>
   );
 }
